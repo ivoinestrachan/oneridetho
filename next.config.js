44931130e7bba221465881a19b0555c728e0b887
Ivoine <ivoinestrachan242@gmail.com>
2023-11-22 20:53:34 -0500

    test if it works deployed version

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 
   env: {
-    API_KEY: process.env.API_KEY
-
+    API_KEY: process.env.API_KEY,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    NODE_ENV: process.env.NODE_ENV,
+    NEXTAUTH_URL: process.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.NEXTAUTH_SECRET,
   },
 }
 
