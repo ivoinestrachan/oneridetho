b823f897ccccf4ae9d14f17f09c08c7398eff4b6
Ivoine <ivoinestrachan242@gmail.com>
2023-11-27 17:37:38 -0500

    implementing booking a ride but need to remove hardcoded id

--- pages/api/bookings.ts
+++ pages/api/bookings.ts
@@ -1,19 +1,62 @@
+import { PrismaClient } from '@prisma/client';
+import { getSession } from "next-auth/react";
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
+ 
+    const userId = 24; 
+
     try {
       const { pickupLocation, dropoffLocation, fare, passengerCount, paymentMethod } = req.body;
 
-      console.log("Booking Data:", req.body);
-      res.status(200).json({ message: 'Ride booked successfully!' });
-    } catch (error) {
-      res.status(500).json({ message: 'Error booking the ride' });
-    }
+      console.log("Request Body:", { pickupLocation, dropoffLocation, fare, passengerCount, paymentMethod });
+
+      const ride = await prisma.ride.create({
+        data: {
+          pickupTime: new Date(),
+          dropoffTime: null,
+          fare: parseFloat(fare),
+          tip: 5, 
+          userId: userId, 
+          driverId: 1, 
+          status: 'Requested', 
+          pickupLocation: pickupLocation, 
+          dropoffLocation: dropoffLocation,
+          passengerCount: parseInt(passengerCount),
+          isAccepted: false, 
+          isConfirmed: false, 
+          paymentMethod: paymentMethod,
+        }
+      });
+
+      const booking = await prisma.booking.create({
+        data: {
+          rideId: ride.id,
+          userId: userId, 
+          driverId: 1, 
+          createdAt: new Date(),
+          isAccepted: false, 
+        }
+      });
+
+      console.log("Booking Data:", booking);
+      res.status(200).json({ message: 'Ride booked successfully!', bookingId: booking.id, rideId: ride.id });
+    } catch (error: unknown) {
+      let errorMessage = "Error booking the ride";
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.error("Error:", errorMessage);
+      res.status(500).json({ message: errorMessage });
+    }    
   } else {
+    console.log(`Method ${req.method} not allowed`);
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
