91cbef384042897e4f806115841bcbc43d4d09af
Ivoine <ivoinestrachan242@gmail.com>
2023-11-28 17:39:29 -0500

    cleaning up booking page

--- pages/rides/[rideId].tsx
+++ pages/rides/[rideId].tsx
@@ -1,74 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { HiMiniStar } from "react-icons/hi2";
 
-// Define TypeScript interfaces for your data
 interface Driver {
-    name: string;
-    rating: number;
+  photoUrl: string;
+  name: string;
+  rating: number;
+  carType: string;
+  licensePlate: string;
+  carImageUrl: string;
+  phone: string;
 }
 
 interface Ride {
-    pickupLocation: string;
-    dropoffLocation: string;
-    fare: number;
-    driver: Driver;
+  pickupLocation: string;
+  dropoffLocation: string;
+  fare: number;
+  driver: Driver;
 }
 
 const RideDetails = () => {
-    const router = useRouter();
-    const { rideId } = router.query;
-    
+  const router = useRouter();
+  const { rideId } = router.query;
 
-    const [ride, setRide] = useState<Ride | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState('');
+  const [ride, setRide] = useState<Ride | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
-    useEffect(() => {
-        if (rideId) {
-            const fetchRideDetails = async () => {
-                setIsLoading(true);
-                try {
-                    const response = await axios.get(`/api/rides/${rideId}`);
-                    setRide(response.data);
-                } catch (error) {
-                    console.error('Error fetching ride details:', error);
-                    setError('Failed to load ride details.');
-                } finally {
-                    setIsLoading(false);
-                }
-            };
-
-            fetchRideDetails();
+  useEffect(() => {
+    if (rideId) {
+      const fetchRideDetails = async () => {
+        setIsLoading(true);
+        try {
+          const response = await axios.get(`/api/rides/${rideId}`);
+          setRide(response.data);
+        } catch (error) {
+          console.error("Error fetching ride details:", error);
+          setError("Failed to load ride details.");
+        } finally {
+          setIsLoading(false);
         }
-    }, [rideId]);
+      };
 
-    if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error}</p>;
+      fetchRideDetails();
+    }
+  }, [rideId]);
 
-    return (
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="px-2">
+      {ride && (
         <div>
-            <h1>Ride Details</h1>
-            {ride && (
-                <div>
-                    <h2>Ride Information</h2>
-                    <p>Pickup Location: {ride.pickupLocation}</p>
-                    <p>Dropoff Location: {ride.dropoffLocation}</p>
-                    <p>Fare: ${ride.fare}</p>
-       
+          <p>Pickup Location: {ride.pickupLocation}</p>
+          <p>Dropoff Location: {ride.dropoffLocation}</p>
 
-                    {ride.driver && (
-                        <div>
-                            <h2>Driver Information</h2>
-                            <p>Name: {ride.driver.name}</p>
-                            <p>Rating: {ride.driver.rating}</p>
-                           
-                        </div>
-                    )}
+          {ride.driver && (
+            <div className="bottom-10 absolute">
+              <div className="flex items-center">
+                <div className="mt-20 rounded-full flex items-center gap-2 ">
+                  {ride.driver.rating}{" "}
+                  <span>
+                    <HiMiniStar />
+                  </span>
                 </div>
-            )}
+                <Image
+                  src={ride.driver.photoUrl}
+                  alt="pfp"
+                  width={70}
+                  height={70}
+                  className="rounded-full absolute border-r border-black"
+                />
+                <Image
+                  src={ride.driver.carImageUrl}
+                  alt="car"
+                  width={225}
+                  height={225}
+                />
+                <div className="ml-[50px]">
+                  <p className="text-gray-500 text-[18px]">
+                    {ride.driver.name}
+                  </p>
+                  <p className="font-bold text-[24px]">
+                    {ride.driver.licensePlate}
+                  </p>
+                  <p className="text-gray-500 text-[18px]">
+                    {ride.driver.carType}
+                  </p>
+                </div>
+              </div>
+              <a href={`tel:${ride.driver.phone}`}>
+                <div>Contact:</div>
+                <p className="underline text-blue-400 text-[18px]">
+                  {ride.driver.phone}
+                </p>
+              </a>
+            </div>
+          )}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default RideDetails;
