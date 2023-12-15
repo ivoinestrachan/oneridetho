7d764b4b9fde7253a96cdaba5999b32d7ba9e9cd
Ivoine <ivoinestrachan242@gmail.com>
2023-12-15 12:21:30 -0500

    fixed user id hard coded

--- pages/api/bookings.ts
+++ pages/api/bookings.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import { getSession } from "next-auth/react";
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
@@ -8,45 +8,67 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
- 
-    const userId = 24; 
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+    //@ts-ignore
+    if (!session || !session.user || !session.user.id) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    //@ts-ignore
+    const userId = parseInt(session.user.id);
 
     try {
-      const { pickupLocation, dropoffLocation, fare, passengerCount, paymentMethod } = req.body;
+      const {
+        pickupLocation,
+        dropoffLocation,
+        fare,
+        passengerCount,
+        paymentMethod,
+      } = req.body;
 
-      console.log("Request Body:", { pickupLocation, dropoffLocation, fare, passengerCount, paymentMethod });
+      console.log("Request Body:", {
+        pickupLocation,
+        dropoffLocation,
+        fare,
+        passengerCount,
+        paymentMethod,
+      });
 
       const ride = await prisma.ride.create({
         data: {
           pickupTime: new Date(),
           dropoffTime: null,
           fare: parseFloat(fare),
-          tip: 5, 
-          userId: userId, 
-          driverId: 1, 
-          status: 'Requested', 
-          pickupLocation: pickupLocation, 
+          tip: 5,
+          userId: userId,
+          driverId: 1,
+          status: "Requested",
+          pickupLocation: pickupLocation,
           dropoffLocation: dropoffLocation,
           passengerCount: parseInt(passengerCount),
-          isAccepted: false, 
-          isConfirmed: false, 
+          isAccepted: false,
+          isConfirmed: false,
           paymentMethod: paymentMethod,
-        }
+        },
       });
 
       const booking = await prisma.booking.create({
         data: {
           rideId: ride.id,
-          userId: userId, 
-          driverId: 1, 
+          userId: userId,
+          driverId: 1,
           createdAt: new Date(),
-          isAccepted: false, 
-        }
+          isAccepted: false,
+        },
       });
 
       console.log("Booking Data:", booking);
-      res.status(200).json({ message: 'Ride booked successfully!', bookingId: booking.id, rideId: ride.id });
+      res.status(200).json({
+        message: "Ride booked successfully!",
+        bookingId: booking.id,
+        rideId: ride.id,
+      });
     } catch (error: unknown) {
       let errorMessage = "Error booking the ride";
       if (error instanceof Error) {
@@ -54,10 +76,10 @@ export default async function handler(
       }
       console.error("Error:", errorMessage);
       res.status(500).json({ message: errorMessage });
-    }    
+    }
   } else {
     console.log(`Method ${req.method} not allowed`);
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
