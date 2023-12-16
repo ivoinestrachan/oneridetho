89795f69c10b28f0b5cc46601c463990e2d65b15
Ivoine <ivoinestrachan242@gmail.com>
2023-12-16 10:23:56 -0500

    has to wait until driver accepts ride to go to rideId

--- pages/rides/[rideId].tsx
+++ pages/rides/[rideId].tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import useSWR from "swr";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { HiMiniStar } from "react-icons/hi2";
+import Confirmation from "../confirmation";
 
 interface Driver {
   photoUrl: string;
@@ -18,108 +20,66 @@ interface Ride {
   pickupLocation: string;
   dropoffLocation: string;
   fare: number;
-  driver: Driver;
+  driver: Driver | null;
 }
+
+const fetcher = (url: string) => axios.get<Ride>(url).then(res => res.data);
 
 const RideDetails = () => {
   const router = useRouter();
   const { rideId } = router.query;
 
-  const [ride, setRide] = useState<Ride | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    if (rideId) {
-      const fetchRideDetails = async () => {
-        setIsLoading(true);
-        try {
-          const response = await axios.get(`/api/rides/${rideId}`);
-          setRide(response.data);
-        } catch (error) {
-          console.error("Error fetching ride details:", error);
-          setError("Failed to load ride details.");
-        } finally {
-          setIsLoading(false);
-        }
-      };
-
-      fetchRideDetails();
-    }
-  }, [rideId]);
+  const { data: ride, error } = useSWR<Ride, Error>(rideId ? `/api/rides/${rideId}` : null, fetcher);
 
   const cancelRide = async () => {
     try {
-      const response = await axios.post(`/api/rides/cancel/${rideId}`);
+      await axios.post(`/api/rides/cancel/${rideId}`);
       alert("Ride has been canceled");
       router.push('/'); 
-    } catch (error) {
-      console.error("Error cancelling ride:", error);
-      setError("Failed to cancel ride.");
+    } catch (cancelError) {
+      console.error("Error cancelling ride:", cancelError);
     }
   };
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  if (!ride && !error) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div className="px-2">
-      {ride && (
-        <div>
-          <p>Pickup Location: {ride.pickupLocation}</p>
-          <p>Dropoff Location: {ride.dropoffLocation}</p>
-
-          {ride.driver && (
+      {ride ? (
+        ride.driver ? (
+          <div>
+            <p>Pickup Location: {ride.pickupLocation}</p>
+            <p>Dropoff Location: {ride.dropoffLocation}</p>
             <div className="bottom-10 absolute">
               <div className="flex items-center">
-                <div className="mt-20 rounded-full flex items-center gap-2 ">
-                  {ride.driver.rating}{" "}
-                  <span>
-                    <HiMiniStar />
-                  </span>
+                <div className="mt-20 rounded-full flex items-center gap-2">
+                  {ride.driver.rating} <span><HiMiniStar /></span>
                 </div>
-                <Image
-                  src={ride.driver.photoUrl}
-                  alt="pfp"
-                  width={70}
-                  height={70}
-                  className="rounded-full absolute border-r border-black"
-                />
-                <Image
-                  src={ride.driver.carImageUrl}
-                  alt="car"
-                  width={225}
-                  height={225}
-                />
+                <Image src={ride.driver.photoUrl} alt="driver" width={70} height={70} className="rounded-full absolute border-r border-black" />
+                <Image src={ride.driver.carImageUrl} alt="car" width={225} height={225} />
                 <div className="ml-[30px]">
-                  <p className="text-gray-500 text-[18px]">
-                    {ride.driver.name}
-                  </p>
-                  <p className="font-bold text-[24px]">
-                    {ride.driver.licensePlate}
-                  </p>
-                  <p className="text-gray-500 text-[18px]">
-                    {ride.driver.carType}
-                  </p>
+                  <p className="text-gray-500 text-[18px]">{ride.driver.name}</p>
+                  <p className="font-bold text-[24px]">{ride.driver.licensePlate}</p>
+                  <p className="text-gray-500 text-[18px]">{ride.driver.carType}</p>
                 </div>
               </div>
               <div className="flex items-center justify-between w-[98%]">
                 <a href={`tel:4155550132`}>
                   <div>Customer Service</div>
-                  <p className="underline text-blue-400 text-[18px]">
-                  4155550132
-                  </p>
+                  <p className="underline text-blue-400 text-[18px]">4155550132</p>
                 </a>
-
                 <div>
-                  <button className="bg-red-500 py-3 pl-2 pr-2 rounded-md text-white"
-                   onClick={cancelRide}
-                  >Cancel Ride</button>
+                  <button className="bg-red-500 py-3 pl-2 pr-2 rounded-md text-white" onClick={cancelRide}>Cancel Ride</button>
                 </div>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        ) : (
+          <Confirmation />
+        )
+      ) : (
+        <p>No ride details available.</p>
       )}
     </div>
   );
