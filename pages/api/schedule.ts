80371cbade04df571ee15cc6ee7f73e2be029971
Ivoine <ivoinestrachan242@gmail.com>
2023-12-25 10:27:12 -0500

    adding stops to a sched ride

--- pages/api/schedule.ts
+++ pages/api/schedule.ts
@@ -2,57 +2,60 @@ import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handle(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-    if (req.method === "POST") {
-      const {
-        userId,
-        pickupLocation,
-        dropoffLocation,
-        scheduledPickupTime,
-        fare,
-        passengerCount,
-        paymentMethod,
-      } = req.body;
-  
-      const parsedUserId = parseInt(userId);
-  
-      if (
-        isNaN(parsedUserId) ||
-        !pickupLocation ||
-        !dropoffLocation ||
-        !scheduledPickupTime
-      ) {
-        return res.status(400).send("Missing or invalid required fields");
-      }
-  
-      try {
-        const scheduledRide = await prisma.ride.create({
-          data: {
-            userId: parsedUserId, 
-            pickupLocation,
-            dropoffLocation,
-            fare: parseFloat(fare),
-            passengerCount: parseInt(passengerCount),
-            paymentMethod,
-            pickupTime: new Date(scheduledPickupTime),
-            dropoffTime: null,
-            driverId: null,
-            isScheduled: true,
-            status: "Scheduled",
-            isConfirmed: false,
-          },
-        });
-  
-        res.status(200).json(scheduledRide);
-      } catch (error) {
-        console.error("Error scheduling ride:", error);
-        res.status(500).send("Error scheduling ride");
-      }
-    } else {
-      res.setHeader("Allow", ["POST"]);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const {
+      userId,
+      pickupLocation,
+      dropoffLocation,
+      scheduledPickupTime,
+      fare,
+      passengerCount,
+      stops,
+      paymentMethod,
+    } = req.body;
+
+    const parsedUserId = parseInt(userId);
+
+    if (
+      isNaN(parsedUserId) ||
+      !pickupLocation ||
+      !dropoffLocation ||
+      !scheduledPickupTime ||
+      !Array.isArray(stops) ||
+      stops.length > 3 
+    ) {
+      return res.status(400).send("Missing or invalid required fields");
     }
+
+    try {
+      const scheduledRide = await prisma.ride.create({
+        data: {
+          userId: parsedUserId, 
+          pickupLocation,
+          dropoffLocation,
+          fare: parseFloat(fare),
+          passengerCount: parseInt(passengerCount),
+          paymentMethod,
+          pickupTime: new Date(scheduledPickupTime),
+          dropoffTime: null,
+          driverId: null,
+          isScheduled: true,
+          status: "Scheduled",
+          isConfirmed: false,
+          stops: JSON.stringify(stops), 
+        },
+      });
+
+      res.status(200).json(scheduledRide);
+    } catch (error) {
+      console.error("Error scheduling ride:", error);
+      res.status(500).send("Error scheduling ride");
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-  
+}
