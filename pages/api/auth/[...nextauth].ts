349254fe1667eb4960699ba74a8df5fbac6fdd1d
Ivoine <ivoinestrachan242@gmail.com>
2024-03-07 16:14:48 -0800

    fixing camera

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,33 +2,30 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
-import GoogleProvider from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
 
 function mapGender(genderString: string) {
-  switch (genderString?.toLowerCase()) {
+  switch (genderString.toLowerCase()) {
     case 'male':
       return 'Male';
     case 'female':
       return 'Female';
     default:
-      return 'Other';
+      return 'Male';
   }
 }
+
 
 const crypto = require('crypto');
 
 function generateUniqueSessionToken() {
   return crypto.randomBytes(32).toString('hex');
 }
-
 export const authOptions = {
+
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+
 
     CredentialsProvider({
       name: "Credentials",
@@ -37,18 +34,20 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
         name: { label: "Name", type: "text" }, 
         dob: { label: "Date of Birth", type: "date" },
-        gender: { label: "Gender", type: "text" },
+        gender: { label: "Gender", type: "text" } ,
         phoneNumber: { label: "Phone Number", type: "text" }
       },
       authorize: async (credentials) => {
         if (!credentials) return null;
 
-        const account = await prisma.account.findUnique({
+        let account = await prisma.account.findUnique({
           where: { email: credentials.email },
         });
 
         if (!account) {
           const hashedPassword = await bcrypt.hash(credentials.password, 10);
+
+
           const genderEnum = mapGender(credentials.gender);
 
           const user = await prisma.user.create({
@@ -61,84 +60,72 @@ export const authOptions = {
             },
           });
 
-          await prisma.account.create({
+          account = await prisma.account.create({
             data: {
               userId: user.id,
               email: credentials.email,
               password: hashedPassword,
             },
           });
-
-          return { id: user.id.toString(), email: user.email };
         }
 
-        const isPasswordMatch = await bcrypt.compare(credentials.password, account.password);
-        if (!isPasswordMatch) return null;
+        if (account) {
+          const isPasswordMatch = await bcrypt.compare(credentials.password, account.password);
+          if (!isPasswordMatch) {
+         
+            return null;
+          }
 
-        return { id: account.userId.toString(), email: account.email };
+
+          return { id: account.userId.toString(), email: account.email };
+        } else {
+          throw new Error("Unable to create or authenticate account");
+        }          
       },
     }),
   ],
 
   secret: process.env.NEXTAUTH_SECRET,
-
   callbacks: {
     //@ts-ignore
-    async jwt({ token, account, profile }) {
-      if (account && profile) {
-        let user = await prisma.user.findUnique({
-          where: { email: profile.email },
+    async jwt({ token, user }) {
+      if (user) {
+        const sessionToken = generateUniqueSessionToken();
+    
+        await prisma.session.create({
+          data: {
+            userId: parseInt(user.id),
+            sessionToken: sessionToken,
+            expiresAt: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), 
+          },
         });
-
-        if (!user) {
-          user = await prisma.user.create({
-            //@ts-ignore
-            data: {
-              email: profile.email,
-              name: profile.name,
-              photoUrl: profile.picture,
-            },
-          });
-        } else if (account.provider === "google") {
-          user = await prisma.user.update({
-            where: { email: profile.email },
-            data: {
-              name: profile.name,
-              photoUrl: profile.picture,
-           
-            },
-          });
-        }
-
-        token.uid = user.id;
       }
-
       return token;
     },
 
-    //@ts-ignore
+     //@ts-ignore
     async session({ session, token }) {
-      if (token.uid) {
+      if (token.sub) {
         const user = await prisma.user.findUnique({
-          where: { id: token.uid },
+          where: { id: parseInt(token.sub) },
         });
-
+    
         if (user) {
           session.user = {
             ...session.user,
-            id: user.id,
+            id: user.id.toString(),
             name: user.name,
             dob: user.dob,
             gender: user.gender,
             image: user.photoUrl,
             phone: user.phone,
-          };
+          } as any; 
         }
       }
-
+  
       return session;
     },
   },
 };
-//@ts-ignore
+
 export default NextAuth(authOptions);
